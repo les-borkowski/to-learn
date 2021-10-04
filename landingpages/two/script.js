8c8f6979495cf0593cd03bd7d00336f7d18f4b1e
Leszek Borkowski <leszek.borkowski@outlook.com>
2021-10-04 10:38:00 +0100

    Initial commit

---
+++ landingpages/two/script.js
@@ -0,0 +1,63 @@
+const subMenu = document.getElementById('button');
+const subNavbar = document.getElementById('navbar-sub');
+const navRespMenu = document.getElementById('nav-resp-menu');
+const navRespMenuBtn = document.getElementById('nav-resp-menu-btn');
+const navRespSubMenu = document.getElementById('nav-resp-sub-menu');
+const navRespSubMenuBtn = document.getElementById('nav-resp-sub-menu-btn');
+const navRespSubMenuIcon = document.getElementById('nav-resp-menu-icon');
+const navLongMenuIcon = document.getElementById('nav-long-menu-icon');
+
+
+let isMenuOpen = false;
+let isSubMenuOpen = false;
+
+// LONG MENU OPEN
+function handleMouseEnter() {
+  subNavbar.classList.add('navbar-sub-active');
+  navLongMenuIcon.classList.add('sub-menu-icon-active');
+};
+
+function handleMouseLeave() {
+  subNavbar.classList.remove('navbar-sub-active');
+  navLongMenuIcon.classList.remove('sub-menu-icon-active');
+};
+
+// SMALL MENU OPEN
+function handleRespMenuOpen() {
+  if (!isMenuOpen) {
+    isMenuOpen = true;
+    navRespMenu.classList.add('navbar-resp-menu-active');
+  } else {
+    isMenuOpen = false;
+    navRespMenu.classList.remove('navbar-resp-menu-active')
+  }
+}
+
+//  SMALL SUB-MENU OPEN
+function handleRespSubMenuOpen() {
+  if (!isSubMenuOpen) {
+    isSubMenuOpen = true;
+    navRespSubMenu.classList.add('navbar-resp-sub-menu-active');
+    navRespSubMenuIcon.classList.add('sub-menu-icon-active');
+  } else {
+    isSubMenuOpen = false;
+    navRespSubMenu.classList.remove('navbar-resp-sub-menu-active')
+    navRespSubMenuIcon.classList.remove('sub-menu-icon-active');
+  }
+}
+// CLOSE SMALL MENUS IF WINDOW IS RESIZED
+function handleWindowResize() {
+  if (isSubMenuOpen) {
+    handleRespSubMenuOpen();
+  }
+  else if (isMenuOpen) {
+    handleRespMenuOpen();
+  }
+}
+
+
+subMenu.addEventListener('mouseover', handleMouseEnter);
+subNavbar.addEventListener('mouseout', handleMouseLeave);
+navRespMenuBtn.addEventListener('click', handleRespMenuOpen);
+navRespSubMenuBtn.addEventListener('click', handleRespSubMenuOpen);
+window.addEventListener('resize', handleWindowResize);
